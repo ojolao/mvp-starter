973324ba388766139eb62e65b371c24c526f8848
ojolao <jolaosho@Omotayos-MacBook-Pro.local>
2017-03-10 20:38:57 -0800

    add borders to list items with enough space between them in preparation for drag and drop

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ListItem = (props) => (
-  <div>
+  <div style={{borderWidth: 0.5, borderRadius: 5, padding: 5, borderColor: 'black', borderStyle: 'solid', marginBottom: 5}}>
     { props.item.content}
   </div>
 );
