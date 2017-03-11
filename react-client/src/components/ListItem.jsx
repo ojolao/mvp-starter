d7e2cee2e875e186e3e367b31a16af9a27fdee25
ojolao <jolaosho@Omotayos-MacBook-Pro.local>
2017-03-11 13:00:20 -0800

    start implementing drag and drop functions for list items

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
 
-const ListItem = (props) => (
-  <div style={{borderWidth: 0.5, borderRadius: 5, padding: 5, borderColor: 'black', borderStyle: 'solid', marginBottom: 5}}>
-    { props.item.content} <button style={{position: 'absolute', right: 0}} onClick ={() => { props.removeToDo(props.item); }}>X</button>
-  </div>
-);
+class ListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleDragStart (e) {
+    console.log('drag is starting');
+  }
+  handleDragOver(e) {
+    e.preventDefault();
+    console.log('drag is over');
+  }
+  handleDrop(e) {
+    e.preventDefault();
+    console.log('something has been dropped');
+  }
+
+  render () {
+    return (
+        <div draggable="true" onDragStart={this.handleDragStart.bind(this)} onDragEnd={this.handleDragOver.bind(this)} onDrop={this.handleDrop.bind(this)} data-id={this.props.item.id} style={{cursor: 'move', borderWidth: 0.5, borderRadius: 5, padding: 5, borderColor: 'black', borderStyle: 'solid', marginBottom: 5}}>{ this.props.item.content}
+            <button style={{position: 'absolute', right: 0}} onClick ={() => { this.props.removeToDo(this.props.item); }}>X</button>
+        </div>
+    );
+  }
+}
 
 export default ListItem;
+
+// need to make this a class
+
+
+
+  // handleChange(event) {
+  //   this.setState({value: event.target.value});
+  //   event.preventDefault();
+  // }
+
+  // addToList() {
+  //   this.props.addToDo(this.state.value);
+  //   this.setState({value: ''});
+  // }
