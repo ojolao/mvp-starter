e627de69fa86cfebce5bcc423ddd33e94b87ac8c
ojolao <ojolao@gmail.com>
2017-07-31 10:32:41 -0700

    moved dragOver eventListener to ListItem for cleaner dragging

--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -5,7 +5,7 @@ const List = (props) => (
   <div>
     <h4> My To Do List </h4>
     There are { props.items.tasks.length } items.
-    <ul onDragOver={props.dragOver}>
+    <ol>
     { props.items.tasks.map((item, i) => {
         var dragging = (i === props.items.dragging) ? 'dragging' : '';
         return (
@@ -15,11 +15,12 @@ const List = (props) => (
             item={item}
             removeToDo={props.removeToDo}
             dragEnd={props.dragEnd}
+            dragOver={props.dragOver}
             dragStart={props.dragStart}
             />
         );
     }, this)}
-    </ul>
+    </ol>
   </div>
 );
 
